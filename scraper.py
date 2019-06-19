bc4d9a6b25e9cc3204c34c509a9f4cd06e4cadee
amshel <amshelhack3r@gmail.com>
2019-06-19 16:35:49 +0000

    optimise scraper methods

--- scraper.py
+++ scraper.py
@@ -1,71 +1,81 @@
 from bs4 import BeautifulSoup
-import requests, enum
-from directory import MangaDirectory
+import requests
+import enum
+from pprint import pformat
+import logging
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s', level=logging.INFO)
+
 
 class Scraper():
     
-    # the name is the actual manga as represented in the url
-    def __init__(self, name):
-        self.name = name
-        MangaDirectory.deleteTempFile()
-        pass
-
-    @classmethod
-    def getSoup(cls, url):
-        html = requests.get(url)
-        return BeautifulSoup(html.content, 'html.parser')
-
-    
-    def getMangaInfo(self):
-        manga_info = dict(count=0, chapters= [])
-
-        url = Scraper.urlBuilder(UrlType.manga.name, self.name)
-
-        soup = Scraper.getSoup(url)
-
-        chapter_list = soup.find('div', attrs={'class':'chapter-list'})
-
-        chapters = chapter_list.findAll('div', attrs={'class':'row'})
-        manga_info['count'] = len(chapters)
-        for chapter in chapters:
-            link = chapter.find('a')
-            manga_info['chapters'].append({link.get('title'):link.get('href')})
-        return manga_info
-
-    def getImagesUrl(self, num):
-        chapter_number = 'chapter_'+str(num)
-        final_dict = dict(manga= self.name, chapter=chapter_number, url_list=[])
+    @staticmethod
+    def getChapter(obj):
+        chapter_number = 'chapter_'+str(obj.get('chapter'))
+        final_dict = (chapter_number, [])
         
-        url = Scraper.urlBuilder(UrlType.chapter.name, self.name, chapter_number)
+        
+        url = Scraper.urlBuilder(UrlType.chapter.name,
+                                 obj.get('name'), chapter_number)
+
+        logging.info(pformat(url))
         soup = Scraper.getSoup(url)
-        image_div = soup.find('div', attrs={'class':'vung-doc'})
+        image_div = soup.find('div', attrs={'class': 'vung-doc'})
         images = image_div.findAll('img')
-
+        
+        
         for link in images:
-            final_dict['url_list'].append(link.get('src'))
+            final_dict[1].append(link.get('src'))
 
-        MangaDirectory.createTempFile()
-        MangaDirectory.saveDictToTemp(str(final_dict))
+        logging.info(pformat(final_dict))
+        return final_dict
+
 
     # This function will take a string name and return list containing a
     # dictionary of name of the manga and the url
     @staticmethod
     def getSearchResults(name):
         search_results = dict()
-        
+
         fname = name.replace(" ", "_")
         # build sarch url
         url = Scraper.urlBuilder(UrlType.search.name, fname)
-        
+
         soup = Scraper.getSoup(url)
-        
+
         # get the div that contains the info i need
-        story_item = soup.findAll('h3', attrs= {'class':'story_name'})
+        story_item = soup.findAll('h3', attrs= {'class': 'story_name'})
         for item in story_item:
             link = item.find('a')
             search_results[link.string] = link.get('href')
-        
+
+        logging.info('search result: %s', str(search_results))
         return search_results
+
+        
+    @staticmethod
+    def getMangaInfo(name):
+        manga_info = dict(count=0, chapters=[])
+
+        url = Scraper.urlBuilder(UrlType.manga.name, name)
+
+        soup = Scraper.getSoup(url)
+
+        chapter_list = soup.find('div', attrs={'class': 'chapter-list'})
+
+        chapters = chapter_list.findAll('div', attrs={'class': 'row'})
+        manga_info['count'] = len(chapters)
+        for chapter in chapters:
+            link = chapter.find('a')
+            manga_info['chapters'].append({link.get('title'): link.get('href')})
+        
+        # logging.info('manga info: %s', str(manga_info))
+        return manga_info
+
+    
+    @classmethod
+    def getSoup(cls, url):
+        html = requests.get(url)
+        return BeautifulSoup(html.content, 'html.parser')
 
     @classmethod
     def urlBuilder(cls, enumType, *args):
@@ -73,17 +83,9 @@ class Scraper():
         separator = '/'
         for word in args:
             base_url = base_url+separator+word
-        
+
         return base_url
-    
-    @classmethod
-    def getName(cls, url):
-        url_list = url.split('/')
-        last_index = 0
-        if len(url_list) > 0:
-            last_index = len(url_list) - 1
-        
-        return url_list[last_index]
+
 
 class UrlType(enum.Enum):
     search = 'search'
